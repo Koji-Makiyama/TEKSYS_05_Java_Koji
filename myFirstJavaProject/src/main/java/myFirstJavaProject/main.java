e7d218b60bbee277f6af3d8eb23fe941858cd86b
Koji Makiyama <akm822013@outlook.com>
2022-04-15 12:04:12 -0700

    JavaProjectFirstCommit

---
+++ myFirstJavaProject/src/main/java/myFirstJavaProject/main.java
@@ -0,0 +1,15 @@
+package myFirstJavaProject;
+
+public class main {
+    static String myString = "Hurray";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		main myObject = new main();
+        System.out.println(main.myString);
+        myObject.myString = "WHAT!";
+        System.out.println(main.myString);
+        // myObject.newVar = "FLOAT";
+        // System.out.println(myObject.newVar);
+	}
+
+}
